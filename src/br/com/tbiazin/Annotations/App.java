8d15fa2deccea151eb42d5042801245462f5ec5b
Tiago Biazin <tiago.biazin02@gmail.com>
2024-03-23 00:55:39 -0300

    Tarefa modulo 19

--- src/br/com/tbiazin/Annotations/App.java
+++ src/br/com/tbiazin/Annotations/App.java
@@ -1,10 +1,23 @@
-package br.com.tbiazin.Annotations;
-
-import br.com.tbiazin.Annotations.Produto;
-
-public class App {
-    public static void main(String[] args) {
-        Produto produto = new Produto();
-        System.out.println(produto.nome()); 
-    }
-}
+	package br.com.tbiazin.Annotations;
+	
+	import java.lang.annotation.Annotation;
+	import java.lang.reflect.Field;
+	
+	public class App {
+		public static void main(String[] args) {
+			
+	        if (Produto.class.isAnnotationPresent(Tabela.class)) {
+	        	
+	            Tabela tabelaAnnotation = Produto.class.getAnnotation(Tabela.class);
+	            
+	            String nomeTabela = tabelaAnnotation.nome();
+	            
+	            System.out.println("Nome da tabela: " + nomeTabela);
+	            
+	        } else {
+	        	
+	            System.out.println("A classe Produto não possui a annotation @Tabela");
+	            
+	        }
+	    }
+	}
--- src/br/com/tbiazin/Annotations/Produto.java
+++ src/br/com/tbiazin/Annotations/Produto.java
@@ -1,13 +1,17 @@
 package br.com.tbiazin.Annotations;
+import java.lang.annotation.Documented;
 
 import java.lang.annotation.Annotation;
-
 @Tabela(nome = "preco")
 public class Produto implements Tabela {
-
+	@Tabela(nome = "preco")
     public Produto() {
     }
 
+    @Override
+    public String toString() {
+        return "Produto";
+    }
     @Override
     public Class<? extends Annotation> annotationType() {
         return Tabela.class;
